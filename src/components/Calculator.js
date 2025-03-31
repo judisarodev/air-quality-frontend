2feb9d70616a6b35e3d4271c048c6548ce688690
Teach <juanestebanguerrerovera@gmail.com>
2025-03-30 22:19:35 -0500

    hola

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Calculator = () => {
+    return (
+        <div>
+            <h2>Calculadora de riesgos</h2>
+            
+        </div>
+        
+    )
+}
+
+export {Calculator}
