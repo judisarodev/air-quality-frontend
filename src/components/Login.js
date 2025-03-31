28190c078773a315981555ac291c92bd5fdf3c76
juansanchezilab <juan.sanchez@ilab.digital>
2025-03-30 22:15:14 -0500

    Se agrega componete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
+import './../styles/login.css'
 
 const Login = () => {
-    return(<>
-        
-    </>);
+    return(<div className="login__box">
+        <form className="login__form">
+            <label>Correo electrónico</label>
+            <input type="text"/>
+
+            <label>Contraseña</label>
+            <input type="password"/>
+
+            <button>INGRESA CON GOOGLE</button>
+            <button>INGRESAR</button>
+        </form>
+    </div>);
 }
 
 export {Login}
